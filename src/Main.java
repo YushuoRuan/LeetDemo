be57b3e4ce83a81f625718d5cf57c357ee9c3124
yuru4772 <yuru4772@colorado.edu>
2019-06-02 13:05:09 +0800

    transpose

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,17 @@ public class Main {
 
 class Solution {
 
+    public int[][] transpose(int[][] A) {
+        int row = A.length;
+        int col = A[0].length;
+        int[][] result = new int[col][row];
+        for(int i = 0; i<row; i++){
+            for(int j = 0; j<col; j++){
+                result[j][i] = A[i][j];
+            }
+        }
+        return result;
+    }
 
     public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
         int evenSum = 0;
