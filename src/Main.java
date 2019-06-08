6ec4af69f5918bf0cf02f8fd13ab33abeb72cf02
yuru4772 <yuru4772@colorado.edu>
2019-06-08 17:20:52 +0800

    islandPerimeter

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,24 @@ public class Main {
 class Solution {
 
 
+    public int islandPerimeter(int[][] grid) {
+        int counter = 0;
+        for(int i = 0; i<grid.length; i++){
+            for(int j = 0; j<grid[0].length; j++){
+                if(grid[i][j]==0)
+                    continue;
+                if(i-1<0||grid[i-1][j]==0)
+                    counter++;
+                if(i+1>grid.length-1||grid[i+1][j]==0)
+                    counter++;
+                if(j-1<0||grid[i][j-1]==0)
+                    counter++;
+                if(j+1>grid[0].length-1||grid[i][j+1]==0)
+                    counter++;
+            }
+        }
+        return counter;
+    }
 
     public int calPoints(String[] ops) {
         int valid1 = 0;
